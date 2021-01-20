4ada3d88fd386c6b115ff409cb9daab0ed49e1d1
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-20 19:45:26 +0530

    [Arti Dandge] Refactor : UI For Headling, Measurement card and cards for conversion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,33 +2,72 @@ import React, {Component} from 'react';
 import {AppBar} from '@material-ui/core';
 import './home.css';
 import Card from 'react-bootstrap/Card';
+import length from '../assets/length.png'
+import temperature from '../assets/temperature.png'
+import volumn from '../assets/volumn.png'
+
 class Home extends Component{
     render(){
         return(
-            <div className="homeContainer">
-            <AppBar id="welcomeBar">
-                <h1>Welcome to Quantity Measurement </h1>
-            </AppBar>
-            <div className="cardContainer">
-                <Card id="Length">
-                    <img id="lengthImg"></img>
-                    <label htmlFor="lenght" id="lengthLable">Length</label>
-                </Card>               
-                <Card id="Temperature">
-                    <img id="temperatureImg"></img>
-                    <label htmlFor="temparature" id="temperatureLable">Temperature</label>
-                </Card>
-                <Card id="Volumn">
-                    <img id="volumnImg"></img>
-                    <label htmlFor="volumn" id="volumnLable">Volumn</label>
-                </Card>        
-                   
-            </div>   
-            <div className="converterContainer">
-                <Card id="convertFromContainer"></Card>    
-                <Card id="convertToContainer"></Card>   
-            </div>                         
-            </div>
+                <div className="homeContainer">              
+                    <AppBar id="welcomeBar">
+                        <h1>Welcome to Quantity Measurement </h1>
+                    </AppBar>           
+                    <div id="chooseType">
+                        <lable>CHOOSE TYPE</lable>
+                    </div> 
+                    <div className="converterCard">
+                        <Card id="length">
+                        <div>
+                            <img src={length}/>
+                        </div>
+                        <div>
+                            Length
+                        </div>
+                        </Card>
+                        <Card id="temperature">
+                        <div>
+                            <img src={temperature}/>
+                        </div>
+                        <div>
+                            Temperature
+                        </div>
+                        </Card>
+                        <Card id="volumn">
+                        <div>
+                            <img src={volumn}/>
+                        </div>
+                        <div>
+                            Volumn
+                        </div>
+                        </Card>
+                    </div>
+                    <div className="calculationContainer">
+                    <Card>                        
+                        <div id="fromLable">
+                            From
+                        </div>
+                        <div id="From">
+                            <div id="inputFrom">
+                                <input type="text"></input>
+                            </div>
+                        </div>
+                        </Card>
+                        <Card>
+                        <div id="toLable">
+                           To
+                        </div>
+                        <div id="To">
+                            <div id="inputTo">
+                                <input type="text"></input>
+                            </div>
+                            <div id="inputToConverter">
+                                
+                            </div>    
+                        </div>
+                        </Card>
+                    </div>                     
+                </div>  
         )
     }
 }

9317a7eea9045e930fe8230a7f4e61489f2cda41
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-21 15:21:56 +0530

    [Arti Dandge] Refactor: UI for Home Page, Removing unwanted dropdown

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,55 +74,7 @@ class Home extends Component{
                         <FormLabel>Volumn</FormLabel>
                         </div>
                         </Card>
-                    </div>
-                    <div className="calculationContainer">
-                    <Card className="FromCardContainer">                        
-                        <div id="fromLable">
-                            <FormLabel>From</FormLabel>
-                        </div>
-                        <div id="From">
-                            <div id="inputFrom">
-                                <TextField id="TextFieldFrom" type="number" variant="outlined" ></TextField>
-                            </div>
-                            <div id="selectFrom">
-                                <Select id="FromDropdown" value={this.state.length} onChange={this.handleChange}>
-                                    <MenuItem value=""><em>None</em></MenuItem>
-                                    <MenuItem value={20}>Kilometre</MenuItem>
-                                    <MenuItem value={20}>Metres</MenuItem>
-                                    <MenuItem value={10}>Centimetres</MenuItem>
-                                    <MenuItem value={30}>Milimetre</MenuItem>
-                                    <MenuItem value={10}>Micrometre</MenuItem>
-                                    <MenuItem value={40}>Mile</MenuItem>
-                                    <MenuItem value={50}>Foot</MenuItem>
-                                    <MenuItem value={60}>Inch</MenuItem>
-                                </Select>   
-                            </div>
-                        </div>
-                        </Card>
-                        <Card>
-                        <div id="toLable">
-                        <FormLabel>To</FormLabel>
-                        </div>
-                        <div id="To">
-                            <div id="inputTo">
-                                <TextField id="TextFieldTo" type="number" variant="outlined" ></TextField>
-                            </div>
-                            <div id="selectTo">
-                            <Select id="ToDropdown">
-                                    <MenuItem value=""><em>None</em></MenuItem>
-                                    <MenuItem value={100}>Kilometre</MenuItem>
-                                    <MenuItem value={200}>Metres</MenuItem>
-                                    <MenuItem value={100}>Centimetres</MenuItem>
-                                    <MenuItem value={300}>Milimetre</MenuItem>
-                                    <MenuItem value={100}>Micrometre</MenuItem>
-                                    <MenuItem value={400}>Mile</MenuItem>
-                                    <MenuItem value={500}>Foot</MenuItem>
-                                    <MenuItem value={600}>Inch</MenuItem>
-                                </Select>   
-                            </div>    
-                        </div>
-                        </Card>
-                    </div>                     
+                    </div>     
                 </div>  
         )
     }

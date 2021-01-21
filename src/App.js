9d04fcd5b4bdfc900a7e4ef29c7fd4de2d78430f
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-21 14:28:13 +0530

    [Arti Dandge] Add: UI For Temperature Converter along with css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router,  Switch,  Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Temperature from './components/Temperature/Temperature';
 
+ 
 function App() {
   return (
     <div className="App">
       <Router>
       <Switch>
-          <Route path="/" component={Home} />
+          <Route path="/" exact component={Home} />
+          <Route path="/temperature" exact component={Temperature} />
       </Switch>
       </Router>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,49 @@
 import React, {Component} from 'react';
-import {AppBar, TextField, Select, MenuItem} from '@material-ui/core';
+import {AppBar, TextField, Select, MenuItem, FormLabel} from '@material-ui/core';
 import './home.css';
 import Card from 'react-bootstrap/Card';
 import length from '../assets/length.png'
 import temperature from '../assets/temperature.png'
 import volumn from '../assets/volumn.png'
-
+import {withRouter} from 'react-router-dom'
 
 class Home extends Component{
+    constructor(){
+        super();
+        this.state={
+            length: '',
+            lengthOne: '',
+        };
+    }
+
+    handleChange=(event)=>{
+        let length=event.target.value;
+        this.setState({
+            length:event.target.value
+        })
+        console.log("Length(From): ", length);
+    }
+
+    // handleChange=(event)=>{
+    //     let lengthOne=event.target.value;
+    //     this.setState({
+    //         lengthOne:event.target.value
+    //     })
+    //     console.log("Length(To): ", lengthOne);
+    // }
+
+    handleTemperature=()=>{
+        this.props.history.push('/temperature');
+    }
+
     render(){
         return(
                 <div className="homeContainer">              
                     <AppBar id="welcomeBar">
-                        <h1>Welcome to Quantity Measurement </h1>
+                        <h1>Welcome to Quantity Measurement</h1>
                     </AppBar>           
                     <div id="chooseType">
-                        <lable>CHOOSE TYPE</lable>
+                    <label>CHOOSE TYPE</label>
                     </div> 
                     <div className="converterCard">
                         <Card id="length">
@@ -23,15 +51,15 @@ class Home extends Component{
                             <img src={length}/>
                         </div>
                         <div>
-                            <lable>Length</lable>
+                        <FormLabel>Length</FormLabel>
                         </div>
                         </Card>
-                        <Card id="temperature">
+                        <Card id="temperature" onClick={this.handleTemperature}>
                         <div>
                             <img src={temperature}/>
                         </div>
                         <div>
-                            <lable>Temperature</lable>
+                        <FormLabel>Temperature</FormLabel>
                         </div>
                         </Card>
                         <Card id="volumn">
@@ -39,21 +67,21 @@ class Home extends Component{
                             <img src={volumn}/>
                         </div>
                         <div>
-                            <lable>Volumn</lable>
+                        <FormLabel>Volumn</FormLabel>
                         </div>
                         </Card>
                     </div>
                     <div className="calculationContainer">
                     <Card className="FromCardContainer">                        
                         <div id="fromLable">
-                            <lable>From</lable>
+                            <FormLabel>From</FormLabel>
                         </div>
                         <div id="From">
                             <div id="inputFrom">
                                 <TextField id="TextFieldFrom" type="number" variant="outlined" ></TextField>
                             </div>
                             <div id="selectFrom">
-                                <Select id="FromDropdown" onChange={this.handleChange}>
+                                <Select id="FromDropdown" value={this.state.length} onChange={this.handleChange}>
                                     <MenuItem value=""><em>None</em></MenuItem>
                                     <MenuItem value={20}>Kilometre</MenuItem>
                                     <MenuItem value={20}>Metres</MenuItem>
@@ -69,7 +97,7 @@ class Home extends Component{
                         </Card>
                         <Card>
                         <div id="toLable">
-                           <lable>To</lable>
+                        <FormLabel>To</FormLabel>
                         </div>
                         <div id="To">
                             <div id="inputTo">
@@ -78,14 +106,14 @@ class Home extends Component{
                             <div id="selectTo">
                             <Select id="ToDropdown">
                                     <MenuItem value=""><em>None</em></MenuItem>
-                                    <MenuItem value={20}>Kilometre</MenuItem>
-                                    <MenuItem value={20}>Metres</MenuItem>
-                                    <MenuItem value={10}>Centimetres</MenuItem>
-                                    <MenuItem value={30}>Milimetre</MenuItem>
-                                    <MenuItem value={10}>Micrometre</MenuItem>
-                                    <MenuItem value={40}>Mile</MenuItem>
-                                    <MenuItem value={50}>Foot</MenuItem>
-                                    <MenuItem value={60}>Inch</MenuItem>
+                                    <MenuItem value={100}>Kilometre</MenuItem>
+                                    <MenuItem value={200}>Metres</MenuItem>
+                                    <MenuItem value={100}>Centimetres</MenuItem>
+                                    <MenuItem value={300}>Milimetre</MenuItem>
+                                    <MenuItem value={100}>Micrometre</MenuItem>
+                                    <MenuItem value={400}>Mile</MenuItem>
+                                    <MenuItem value={500}>Foot</MenuItem>
+                                    <MenuItem value={600}>Inch</MenuItem>
                                 </Select>   
                             </div>    
                         </div>
@@ -96,4 +124,4 @@ class Home extends Component{
     }
 }
 
-export default Home;
+export default withRouter(Home);

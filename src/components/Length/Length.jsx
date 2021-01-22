90274499bdc85648144796bdbdd478a459d66ab1
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-22 19:03:37 +0530

    [Arti Dandge] Add: Length Conversion calculations

--- src/components/Length/Length.jsx
+++ src/components/Length/Length.jsx
@@ -5,10 +5,21 @@ import './length.css'
 import length from '../../assets/length.png'
 import temperature from '../../assets/temperature.png'
 import volumn from '../../assets/volumn1.png'
+import LengthConverter from '../../services/LengthConverter';
+
+let lengthObject = new LengthConverter();
+function Calculate(from, to, fromtext) {
+    console.log(from + ','+ to + ','+ fromtext);
+}
+var result = 0;
+lengthObject.Calculate();
+
 class Length extends Component{
     constructor(){
         super();
+        this.state={setFromLength:'', setToLength:'', setFromTextLength:''};
     }
+    
     handleTemperature=()=>{
         this.props.history.push('/temperature');
     }
@@ -21,6 +32,26 @@ class Length extends Component{
         this.props.history.push('/volumn');
     }
 
+    handleChangeFrom=(event)=>{
+        this.setState({ setFromLength:event.target.value});
+        this.state.setFromLength = event.target.value;
+        this.result = lengthObject.Calculate(this.state.setFromTextLength, this.state.setFromLength, this.state.setToLength);
+        console.log("Length value from dropdown: ", this.state.setFromLength);
+        console.log("conversion result (From): "+ this.result);
+    }
+
+    handleChangeTo=(event)=>{
+        this.setState({setToLength:event.target.value});
+        this.state.setToLength = event.target.value;
+        this.result = lengthObject.Calculate(this.state.setFromTextLength, this.state.setFromLength, this.state.setToLength);
+        console.log("conversion result (To): "+ this.result);
+    }
+
+    textChange=(event)=>{
+         this.setState({setFromTextLength:event.target.value});
+         this.state.setFromTextLength = event.target.value;
+         this.result = lengthObject.Calculate(this.state.setFromTextLength, this.state.setFromLength, this.state.setToLength);
+    }
 
     render(){
         return(
@@ -64,19 +95,19 @@ class Length extends Component{
                         </div>
                         <div id="From">
                             <div id="inputFrom">
-                                <TextField id="TextFieldFrom" type="number" variant="outlined" ></TextField>
+                                <TextField id="TextFieldFrom" type="number" variant="outlined" onChange={this.textChange}></TextField>
                             </div>
                             <div id="selectFrom">
-                                <Select id="FromDropdown">
+                                <Select id="FromDropdown" value={this.state.setFromLength} onChange={this.handleChangeFrom}>
                                     <MenuItem value=""><em>None</em></MenuItem>
-                                    <MenuItem value={20}>Kilometre</MenuItem>
-                                    <MenuItem value={20}>Metres</MenuItem>
-                                    <MenuItem value={10}>Centimetres</MenuItem>
-                                    <MenuItem value={30}>Milimetre</MenuItem>
-                                    <MenuItem value={10}>Micrometre</MenuItem>
-                                    <MenuItem value={40}>Mile</MenuItem>
-                                    <MenuItem value={50}>Foot</MenuItem>
-                                    <MenuItem value={60}>Inch</MenuItem>                                    
+                                    <MenuItem value={39370}>Kilometre</MenuItem>
+                                    <MenuItem value={39}>Metres</MenuItem>
+                                    <MenuItem value={0.39370}>Centimetres</MenuItem>
+                                    <MenuItem value={0.0393701}>Milimetre</MenuItem>
+                                    <MenuItem value={0.0000393701}>Micrometre</MenuItem>
+                                    <MenuItem value={63360}>Mile</MenuItem>
+                                    <MenuItem value={12}>Foot</MenuItem>
+                                    <MenuItem value={1}>Inch</MenuItem>                                    
                                 </Select>   
                             </div>
                         </div>
@@ -87,19 +118,19 @@ class Length extends Component{
                         </div>
                         <div id="To">
                             <div id="inputTo">
-                                <TextField id="TextFieldTo" type="number" variant="outlined" ></TextField>
+                                <TextField id="TextFieldTo" type="number" variant="outlined" value={this.result}></TextField>
                             </div>
-                            <div id="selectTo">
-                            <Select id="ToDropdown">
+                            <div id="selectTo" >
+                            <Select id="ToDropdown" value={this.state.setToLength} onChange={this.handleChangeTo}>
                                 <MenuItem value=""><em>None</em></MenuItem>
-                                <MenuItem value={20}>Kilometre</MenuItem>
-                                <MenuItem value={20}>Metres</MenuItem>
-                                <MenuItem value={10}>Centimetres</MenuItem>
-                                <MenuItem value={30}>Milimetre</MenuItem>
-                                <MenuItem value={10}>Micrometre</MenuItem>
-                                <MenuItem value={40}>Mile</MenuItem>
-                                <MenuItem value={50}>Foot</MenuItem>
-                                <MenuItem value={60}>Inch</MenuItem>                             
+                                <MenuItem value={1}>Kilometre</MenuItem>
+                                <MenuItem value={2}>Metres</MenuItem>
+                                <MenuItem value={3}>Centimetres</MenuItem>
+                                <MenuItem value={4}>Milimetre</MenuItem>
+                                <MenuItem value={5}>Micrometre</MenuItem>
+                                <MenuItem value={6}>Mile</MenuItem>
+                                <MenuItem value={7}>Foot</MenuItem>
+                                <MenuItem value={8}>Inch</MenuItem>                             
                             </Select>   
                             </div>    
                         </div>
---
+++ src/services/LengthConverter.js
@@ -0,0 +1,36 @@
+class LengthConverter {
+    Calculate(value, from, to) {
+        console.log('value: ' + value + '    from: ' + from + '   to: ' + to);
+        let inches= 0;
+        switch (to) {
+            case 1:
+                this.inches = value * from;
+                return this.inches / 39370;
+            case 2:
+                this.inches = value * from;
+                return this.inches / 39.37;
+            case 3:
+                this.inches = value * from;
+                return this.inches * 2.54;
+            case 4:
+                this.inches = value * from;
+                return this.inches * 25.4;
+            case 5:
+                this.inches = value * from;
+                return this.inches * 25400;
+            case 6:
+                this.inches = value * from;
+                return this.inches / 63360;
+            case 7:
+                this.inches = value * from;
+                return this.inches * 25400;
+            case 8:
+                this.inches = value * from;
+                return this.inches / 12;
+            default:
+                return null;
+        }
+    }
+}
+
+export default LengthConverter;
